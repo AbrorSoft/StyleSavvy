8b3dbb9cae55a5492ec8746b1f36517c59fc217e
AbrorSoft <abroranvarov2@mail.ru>
2023-07-07 22:39:10 +0500

    changes ui

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -5,7 +5,7 @@ const CategoryPreview = ({title, products})=>{
     return(
         <div className='category-preview-container'>
             <h2>
-                <Link className='title' to={title}>{title.toUpperCase()}</Link>
+                <Link onClick={()=>  window.scrollTo(500, 0) } className='title' to={title}>{title.toUpperCase()}</Link>
                 </h2>
                 <div className='preview'>
                     {
--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -3,7 +3,10 @@ import { useNavigate } from 'react-router-dom'
 const CategoryItem = ({category})=>{
     const  {imageUrl, title, route} =category
     const navigate = useNavigate()
-    const onNavigateHedler = ()=> navigate(route)
+    const onNavigateHedler = ()=> {
+      navigate(route)
+      window.scrollTo(500, 0);
+    }
     return (
       <div className="directory-item-container" onClick={onNavigateHedler}>
               <div className="background-image" style={{backgroundImage:`url(${imageUrl})`}}/>
